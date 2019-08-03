6c0cdfadd1e9bb062e5c1ca6d6cc1bdcc4fe8e8b
martokolio <52069803+martokolio@users.noreply.github.com>
2019-08-03 20:11:35 +0500

    Add files via upload

---
+++ windoks/допилить.py
@@ -0,0 +1,15 @@
+print("результат" + c)
+
+
+if what == "+":
+	c=a+b
+	print("результат"+str(с))
+ elif what == "-":
+ 	c=a-b
+ 	print("результат"+str(с))
+
+ else:
+ 	print("я не знаю токого знака")
+
+
+
